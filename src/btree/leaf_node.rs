9808345b7df87232ced016f6c836d790229daae9
Aidan Bailey <dev@aidanbailey.me>
2024-06-29 22:54:38 +0200

    Add getters

--- src/btree/leaf_node.rs
+++ src/btree/leaf_node.rs
@@ -7,7 +7,20 @@ pub struct LeafNode<T: Clone> {
 }
 
 impl<T: Clone> LeafNode<T> {
+
+    /// Construct a new leaf node with the given key and value
     pub fn new(key: usize, value: T) -> LeafNode<T> {
         LeafNode { key, value }
     }
+
+    /// Get the key of the leaf node
+    pub fn key(&self) -> usize {
+        self.key
+    }
+
+    /// Get the key of the leaf node
+    pub fn value(&self) -> &T {
+        &self.value
+    }
+
 }
