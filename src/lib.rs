07be7c41fc512db73e737f0e8ade296c22186c52
Aidan Bailey <dev@aidanbailey.me>
2024-06-29 22:44:54 +0200

    Add insert test

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,11 @@ mod tests {
         let btree = BTree::<i32>::new(1);
         assert_eq!(1, btree.k())
     }
+
+    #[test]
+    fn insert_into_empty_btree() {
+        let btree = BTree::<i32>::new(1);
+        btree.insert(1, 1);
+        // TODO: Flesh this out
+    }
 }
