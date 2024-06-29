dce5c2bd4d57a934a99848405b99e70d3a5e2226
Aidan Bailey <dev@aidanbailey.me>
2024-06-29 23:37:19 +0200

    Refactor

--- src/btree/internal_node.rs
+++ src/btree/internal_node.rs
@@ -6,25 +6,25 @@ use super::leaf_node::LeafNode;
 /// BTree internal node
 pub struct InternalNode<T: Clone> {
     k: usize,
-    keys: Vec<Option<usize>>,
-    children: Vec<Option<InternalNode<T>>>,
-    leaves: Vec<Option<LeafNode<T>>>,
+    keys: Vec<usize>,
+    children: Vec<InternalNode<T>>,
+    leaves: Vec<LeafNode<T>>,
 }
 
 impl<T: Clone> InternalNode<T> {
     pub fn new(k: usize) -> InternalNode<T> {
         InternalNode {
             k,
-            keys: vec![None; k],
-            children: vec![None; k],
-            leaves: vec![None; k],
+            keys: Vec::new(),
+            children: Vec::new(),
+            leaves: Vec::new(),
         }
     }
 
     pub fn with_keyval(k: usize, key: usize, value: T) -> InternalNode<T> {
         let mut node = InternalNode::<T>::new(k);
-        node.keys[0] = Some(key);
-        node.leaves[0] = Some(LeafNode::<T>::new(key, value));
+        node.keys.push(key);
+        node.leaves.push(LeafNode::new(key, value));
         node
     }
 }
